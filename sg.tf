bf3d50cd13231ecadeeef622d3e24c5a125f1709
jahnaviperumalla <94068672+jahnaviperumalla@users.noreply.github.com>
2022-12-16 16:01:19 +0530

    Update sg.tf

--- sg.tf
+++ sg.tf
@@ -1,3 +1,19 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "aws" {
+    region = "us-west-2"
+  
+}    
+
+
+
 variable "port_values" {
 	type = list
 	default = ["port_list"]
@@ -5,12 +21,11 @@ variable "port_values" {
 
 resource "aws_security_group" "main" {
    count = length(var.port_values)
-   name   = port-$[count.index]
-   vpc_id = data.aws_vpc.main.id
+   name   = "security"
+   vpc_id = "vpc_id"
    ingress {
-       description = ingress.value.description
-       from_port   = element(var.port_list,count.index)
-       to_port     = element(var.port_list,count.index)
+       from_port   = element(var.port_values,count.index)
+       to_port     = element(var.port_values,count.index)
        protocol    = "tcp"
        cidr_blocks = ["0.0.0.0/0"]
 
