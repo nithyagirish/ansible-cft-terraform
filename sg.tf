ce8197f6853e11d80a2272cbd4f2d2b407f10da9
jahnaviperumalla <jahnaviperumalla@gmail.com>
2022-12-16 15:51:35 +0530

    ninth commit

--- sg.tf
+++ sg.tf
@@ -7,12 +7,21 @@ resource "aws_security_group" "main" {
    count = length(var.port_values)
    name   = port-$[count.index]
    vpc_id = data.aws_vpc.main.id
-   description = ingress.value.description
-   from_port   = element(var.port_list,count.index)
-   to_port     = element(var.port_list,count.index)
-   protocol    = "tcp"
-   cidr_blocks = ["0.0.0.0/0"]
-      
+   ingress {
+       description = ingress.value.description
+       from_port   = element(var.port_list,count.index)
+       to_port     = element(var.port_list,count.index)
+       protocol    = "tcp"
+       cidr_blocks = ["0.0.0.0/0"]
+
+   }
+   egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }      
    
 
    tags = {
