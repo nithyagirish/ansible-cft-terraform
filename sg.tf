b2ae75234f90f7c52f887998008fb3ff5e5cc5fc
jahnaviperumalla <jahnaviperumalla@gmail.com>
2022-12-19 18:09:47 +0530

    30 commit

--- sg.tf
+++ sg.tf
@@ -5,16 +5,15 @@ variable "port_values" {
 
 resource "aws_security_group" "main" {
    vpc_id = "vpc_id1"
-   dynamic "ingress" {
-     for_each = toset(var.port_values)
-     content {
+   for_each = toset(var.port_values)
+   ingress {
        from_port   = each.value
        to_port     = each.value
        protocol    = "tcp"
        cidr_blocks = ["0.0.0.0/0"]
 
    }
-   }   
+     
    egress {
     from_port        = 0
     to_port          = 0
