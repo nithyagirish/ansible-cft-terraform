01f9958ad6b3a78e31d847ecadb5b14640c7d57c
jahnaviperumalla <jahnaviperumalla@gmail.com>
2022-12-19 15:59:08 +0530

    17 commit

--- sg.tf
+++ sg.tf
@@ -1,38 +1,20 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.0"
-    }
-  }
-}
-
-provider "aws" {
-    region = "us-west-2"
-  
-}    
-
-
-
 variable "port_values" {
 	type = list
-	default = ["80","443"]
+	default = port_list
 }
 
 resource "aws_security_group" "main" {
-   count = length(var.port_values)
    name   = "security_name"
    vpc_id = "vpc_id1"
-   for_each = var.port_values
-   content {
-     ingress {
+   count = length(var.port_values)
+   ingress {
        from_port   = element(var.port_values,count.index)
        to_port     = element(var.port_values,count.index)
        protocol    = "tcp"
        cidr_blocks = ["0.0.0.0/0"]
 
    }
-   }
+   
    egress {
     from_port        = 0
     to_port          = 0
