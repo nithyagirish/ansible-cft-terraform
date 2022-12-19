27af1ea8313ffd01c1e416f0826ed692fea86017
jahnaviperumalla <jahnaviperumalla@gmail.com>
2022-12-19 22:56:18 +0530

    39 commit

--- launchconfig.tf
+++ launchconfig.tf
@@ -4,7 +4,7 @@ resource "aws_launch_configuration" "web" {
   instance_type = "type1"
   key_name = "keypair"
   security_groups = ["security_groups1" ]
-  associate_public_ip_address = " associate_public_ip_address1"
+  associate_public_ip_address = "associate_public_ip_address1"
   user_data = file("data.sh")
   lifecycle {
     create_before_destroy = "create_before_destroy1"
