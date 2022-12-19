fb9c2db236980172129692ee9cc784de2b701306
jahnaviperumalla <jahnaviperumalla@gmail.com>
2022-12-19 14:13:53 +0530

    13 commit

--- volumes.tf
+++ volumes.tf
@@ -1,10 +1,8 @@
 resource "aws_ebs_volume" "sample" {
   availability_zone = "availability_zone1"
   size              = "size1"
-  availability_zone = "availability_zone1"
   encrypted = "encrypted1"
   final_snapshot = "final_snapshot1"
-  iops = "iops1"
   type = "type1"
   tags = {
     Name = "ebs_name"
