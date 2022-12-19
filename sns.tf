620deb3f8f00d0e1dabecf1ece7d5ac6c34e7844
jahnaviperumalla <jahnaviperumalla@gmail.com>
2022-12-19 14:42:58 +0530

    15 commit

--- sns.tf
+++ sns.tf
@@ -1,7 +1,6 @@
 resource "aws_sns_topic" "this" {
 
   name        = "name1"
-  name_prefix = "name_prefix1"
   display_name                             = "display_name1"
   fifo_topic                               = "fifo_topic1"
   content_based_deduplication              = "content_based_deduplication1"
