f7e6458481b2e2e4d8778fdc74a9812d05f70801
jahnaviperumalla <jahnaviperumalla@gmail.com>
2022-12-19 15:51:18 +0530

    16 commit

--- sg.tf
+++ sg.tf
@@ -16,19 +16,22 @@ provider "aws" {
 
 variable "port_values" {
 	type = list
-	default = ["port_list"]
+	default = ["80","443"]
 }
 
 resource "aws_security_group" "main" {
    count = length(var.port_values)
    name   = "security_name"
    vpc_id = "vpc_id1"
-   ingress {
+   for_each = var.port_values
+   content {
+     ingress {
        from_port   = element(var.port_values,count.index)
        to_port     = element(var.port_values,count.index)
        protocol    = "tcp"
        cidr_blocks = ["0.0.0.0/0"]
 
+   }
    }
    egress {
     from_port        = 0
