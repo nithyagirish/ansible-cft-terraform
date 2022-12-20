971205cb0996775b8aabefc818684627b9f72192
jahnaviperumalla <jahnaviperumalla@gmail.com>
2022-12-20 12:25:20 +0530

    46 commit

--- efs.tf
+++ efs.tf
@@ -1,7 +1,6 @@
 resource "aws_efs_file_system" "efs" {
    creation_token = "efs-token"
    performance_mode = "performance_mode1"
-   throughput_mode = "throughput_mode1"
    encrypted = "encryption"
    availability_zone_name= "availability_zone_name1"
    lifecycle_policy {
