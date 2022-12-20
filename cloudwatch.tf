2803750b3a27d3168cdcf918481f1e97e1a5a6ea
jahnaviperumalla <jahnaviperumalla@gmail.com>
2022-12-20 18:04:31 +0530

    66 commit

--- cloudwatch.tf
+++ cloudwatch.tf
@@ -2,12 +2,12 @@
 resource "aws_cloudwatch_metric_alarm" "ec2-alarm" {
   alarm_name                = "name1"
   comparison_operator       = "operator"
-  evaluation_periods        = "evaluation_period"
-  metric_name               = "metric_name"
+  evaluation_periods        = "evaluation_period1"
+  metric_name               = "metric_name1"
   namespace                 = "namespace1"
   period                    = "period1"
   statistic                 = "statistics1"
-  threshold                 = "thresholed1"
-  alarm_description         = "description"
+  threshold                 = "threshold1"
+  alarm_description         = "description1"
 
 }
