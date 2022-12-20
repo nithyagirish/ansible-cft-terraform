20a9e3a93ed88e27f73298c980f82c06d16e5633
jahnaviperumalla <jahnaviperumalla@gmail.com>
2022-12-20 10:39:07 +0530

    44 commit

--- s3.tf
+++ s3.tf
@@ -4,7 +4,7 @@ resource "aws_s3_bucket" "temp" {
     bucket = "bucket_name" 
     acl = "access"   
     versioning {
-      enabled = "enable"
+      enabled = "enable1"
   }
     force_destroy= "force_destroy1"
 
