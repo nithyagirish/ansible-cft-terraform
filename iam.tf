240f5fc0153e830d3d9135645424390b44d4e445
jahnaviperumalla <jahnaviperumalla@gmail.com>
2022-12-21 18:52:21 +0530

    82 commit

--- iam.tf
+++ iam.tf
@@ -1,10 +1,17 @@
 variable "user_names" {
   description = "IAM usernames"
   type        = list(string)
-  default     = "users_name"
+  default     = users_name
 }
 
 resource "aws_iam_user" "example" {
   count = length(var.user_names)
   name  = var.user_names[count.index]
+}
+
+resource "aws_iam_policy_attachment" "test-attach" {
+  name       = "test-attachment"
+  count = length(var.user_names)
+  user = element(var.user_names,count.index)
+  policy_arn = "policy_arn1"
 }
