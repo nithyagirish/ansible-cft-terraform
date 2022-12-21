c79a6f61e4fac8cf07aa646220061bdb04330dec
jahnaviperumalla <jahnaviperumalla@gmail.com>
2022-12-21 15:13:41 +0530

    72 commit

--- ec2.tf
+++ ec2.tf
@@ -4,6 +4,6 @@ resource "aws_instance" "myinstance" {
    associate_public_ip_address = "associate_public_ip_address1"
    availability_zone= "availability_zone1"
    key_name = "keyname1"
-   security_groups="security_groups1"
+   vpc_security_group_ids =["security_groups1"]
    subnet_id= "subnet_id1"
 }
